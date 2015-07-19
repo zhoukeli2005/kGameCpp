5d1608a52d96f73ea89b8e30f12eae8b243eb6a5
Keli Zhou <zhoukeli2005@163.com>
2015-07-19 15:40:46 +0800

    init

---
+++ proj.ios/kGameCpp/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  kGame
+//
+//  Created by qiaokeli on 15/6/6.
+//  Copyright (c) 2015年 qiaokeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, retain) UIWindow * window;
+
+@end
+
