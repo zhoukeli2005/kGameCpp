4ca4a04de54c175215d6199dcaaadf24039fa2d6
Keli Zhou <zhoukeli2005@163.com>
2015-07-19 15:19:45 +0800

    Initial Commit

---
+++ kGameCpp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kGameCpp
+//
+//  Created by qiaokeli on 15/7/19.
+//  Copyright (c) 2015年 qiaokeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kGameCpp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kGameCpp
+//
+//  Created by qiaokeli on 15/7/19.
+//  Copyright (c) 2015年 qiaokeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
